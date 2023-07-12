7a785f880813f3c36553c158e9c3edbf286bf7e4
Dipesh Dhandha <55910147+DipeshDhandha07@users.noreply.github.com>
2023-07-12 11:15:00 +0530

    Update eye_detector.py

--- Face detector ,Motion detector and gaming programs/eye_detector.py
+++ Face detector ,Motion detector and gaming programs/eye_detector.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_eye.xml')
 
 # Check if the face cascade file has been loaded properly
 if face_cascade.empty():
